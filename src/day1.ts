72d903c42d79d8321f45505cd5dd677bc86d4093
Kolya Venturi <me@kolya.co>
2024-12-02 13:45:16 -0700

    Day 1 part 1 in node because it's easy

---
+++ src/day1.ts
@@ -0,0 +1,16 @@
+import path from 'node:path';
+import fs from 'node:fs';
+
+const file = path.join('input/day1.txt');
+const content = fs.readFileSync(file, 'utf8');
+
+const lines = content.split('\n').map(l => l.replace(/\s+/g, ' ')).filter(l => l.trim().length > 0);
+const numbers = lines.map((line) => line.split(' ').map((num) => parseInt(num, 10)));
+
+const sortLeft = numbers.map(([a]) => a).sort();
+const sortRight = numbers.map(([, b]) => b).sort();
+
+const diff = sortLeft.map((a, i) => Math.abs(a - sortRight[i]));
+const sum = diff.reduce((a, b) => a + b, 0);
+
+console.log('Part 1:', sum);
