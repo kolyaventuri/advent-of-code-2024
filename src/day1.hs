362df392f1a991d27dd641f890658592bf5290d6
Kolya Venturi <me@kolya.co>
2024-12-02 15:25:31 -0700

    Part 2

--- src/day1.hs
+++ src/day1.hs
@@ -1,13 +1,21 @@
 import Util.Split (splitOn)
 import Data.List (sort)
+import qualified Data.Map as Map
+import Data.Bits (Bits(xor))
 
 main :: IO ()
 
 getDistance :: ([Int], [Int]) -> [Int]
 getDistance (left, right) = zipWith subtract left right
 
+countOccurrences :: (Ord a) => [a] -> Map.Map a Int
+countOccurrences = foldr (\x acc -> Map.insertWith (+) x 1 acc) Map.empty
+
+getSimilarity :: [Int] -> Map.Map Int Int -> [Int]
+getSimilarity list counts = map (\x -> Map.findWithDefault 0 x counts * x) list
+
 main = do
-  contents <- readFile "input/day1.test.txt"
+  contents <- readFile "input/day1.txt"
   let lines = splitOn "\n" contents
   let parts = filter (not . null) (map words lines)
   let numbers = map (map read :: [String] -> [Int]) parts
@@ -19,5 +27,14 @@ main = do
   let part1 = sum distance
 
   putStrLn $ "Part 1: " ++ show part1
+
+  let counts = countOccurrences sortRight
+  let similarities = getSimilarity sortLeft counts
+
+  let part2 = sum similarities
+
+  putStrLn $ "Part 2: " ++ show part2
+
+
 
 
