2a18c779b47b8eb5ac63abd1fead281d1cb6283e
Kolya Venturi <me@kolya.co>
2024-12-02 13:47:54 -0700

    A simple solution for day1 part 2

--- src/day1.ts
+++ src/day1.ts
@@ -14,3 +14,16 @@ const diff = sortLeft.map((a, i) => Math.abs(a - sortRight[i]));
 const sum = diff.reduce((a, b) => a + b, 0);
 
 console.log('Part 1:', sum);
+
+const count: Record<number, number> = {};
+
+for (const n of sortRight) {
+  count[n] = (count[n] || 0) + 1;
+}
+
+let sum2 = 0;
+for (const n of sortLeft) {
+  sum2 += n * (count[n] || 0);
+}
+
+console.log('Part 2:', sum2);
