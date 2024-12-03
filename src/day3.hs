0705067622ef295252f47086272432f5ac7577cc
Kolya Venturi <me@kolya.co>
2024-12-03 07:05:27 -0700

    Day 3: Part 1

---
+++ src/day3.hs
@@ -0,0 +1,41 @@
+import Util.Split (splitOn)
+import Data.Char (isDigit)
+
+main :: IO ()
+
+extractNumbers :: String -> [Int]
+extractNumbers [] = []
+extractNumbers input = map (read :: String -> Int) parts
+  where
+    parts = splitOn "," input
+
+readArgs :: (String, Int) -> [Int]
+readArgs ([], _) = []
+readArgs (input, index)
+  | index == length input - 1 = []
+  | input !! index == ')' = extractNumbers (take index input)
+  | not (isDigit (input !! index)) && input !! index /= ',' = []
+  | otherwise = readArgs (input, index + 1)
+
+readInstructions :: (String, String) -> [[Int]]
+readInstructions (input, name) = processChunks input
+  where
+    instructionLength = length name + 1 -- Add one to account for first parenthesis
+    processChunks [] = []
+    processChunks str
+      | length str < instructionLength = [[]]
+      | take (length name) str == name = readArgs (drop instructionLength str, 0) : processChunks (drop instructionLength str)
+      | otherwise = processChunks (drop 1 str)
+
+main = do
+  contents <- readFile "input/day3.test.txt"
+  let raw = splitOn "\n" contents
+  let lines = filter (not . null) raw
+
+  let rawInstructions = map (\line -> readInstructions (line, "mul")) lines
+  let filteredInstructions = map (filter (not . null)) rawInstructions
+  let evaluated = map (map product) filteredInstructions
+  let part1 = sum $ concat evaluated
+
+  putStrLn $ "Part 1: " ++ show part1
+
