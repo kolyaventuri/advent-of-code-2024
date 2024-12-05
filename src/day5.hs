74cd61d056bd59da76dcbaf2ef099f7dc13b2fce
Kolya Venturi <me@kolya.co>
2024-12-05 07:05:34 -0700

    Start somethig for day5

--- src/day5.hs
+++ src/day5.hs
@@ -2,6 +2,24 @@ import Util.String (splitOn)
 
 main :: IO ()
 
+filterOrdering :: ([[Int]], [Int], [Int]) -> [Int]
+filterOrdering ([], [], _) = []
+filterOrdering ([], _, _) = []
+filterOrdering (ordering, pages, stack)
+  | length stack == length pages = stack
+  | otherwise = filterOrdering (ordering, pages, stack ++ [next])
+  where
+    next = pages !! length stack
+    current = if null stack then head pages else pages !! (length stack - 1)
+    rules = all (\x -> head x == next) ordering
+
+part1Ordering :: ([[Int]], [[Int]], [Int]) -> [Int]
+part1Ordering ([], [], _) = []
+part1Ordering ([], _, _) = []
+part1Ordering (ordering, pages, stack)
+  | length stack == length pages = stack
+  | otherwise = []
+
 main = do
   contents <- readFile "input/day5.test.txt"
   let chunks = splitOn "\n\n" contents
@@ -17,3 +35,10 @@ main = do
 
   print ordering
   print pages 
+
+  let part1 = part1Ordering (ordering, pages, [])
+
+  putStrLn ""
+  print (filterOrdering (ordering, head pages, []))
+
+  putStrLn $ "Part 1: " ++ show part1
