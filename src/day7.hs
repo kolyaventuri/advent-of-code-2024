a579ab6729db5f2b7d3658cdc5f2cbf39b9070f2
Kolya Venturi <me@kolya.co>
2024-12-07 17:47:04 -0700

    Day 7: Part 1

--- src/day7.hs
+++ src/day7.hs
@@ -1,21 +1,28 @@
 import Util.String (splitOn, slice)
-import qualified Data.Map as Map
+import Debug.Trace (trace)
 
 main :: IO ()
 
 toInt :: String -> Int
 toInt str = read str :: Int
 
+checkEquationValidity :: (Int, [Int]) -> Bool
+checkEquationValidity (result, numbers)
+  -- | trace (show result ++ " " ++ show numbers) False = undefined
+  | null numbers = abs result < 2 
+  | mod result (head numbers) == 0 = checkEquationValidity (div result (head numbers), tail numbers) || checkEquationValidity (result - head numbers, tail numbers)
+  | otherwise = checkEquationValidity (result - head numbers, tail numbers)
+
 main = do
-  contents <- readFile "input/day7.test.txt"
+  contents <- readFile "input/day7.txt"
   let lines = splitOn "\n" contents
   let filtered = filter (not . null) (map words lines)
-  
-  let cleaned = map (\x -> (toInt (slice 0 (length (head x) - 1) (head x)), map toInt (tail x))) filtered
-  let map = Map.fromList cleaned
 
-  let x = Map.lookup 3267 map
+  let cleaned = map (\x -> (toInt (slice 0 (length (head x) - 1) (head x)), map toInt (reverse (tail x)))) filtered
 
-  print x 
+  let valid = filter checkEquationValidity cleaned
 
+  let part1 = sum (map fst valid)
+
+  putStrLn $ "Part 1: " ++ show part1
 
